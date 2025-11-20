e8ca9b756199604bb1b9533ca1e7a2b333ca1035
IronTenshi <2356128385@qq.com>
2025-11-20 22:16:55 +0800

    Update test3.java

--- src/Deemo1/test3.java
+++ src/Deemo1/test3.java
@@ -1,5 +1,13 @@
 package Deemo1;
 
-public class test3 {
+import java.util.StringJoiner;
 
+public class test3 {
+	public static void main(String[] args) {
+		StringJoiner sj = new StringJoiner("---");
+		sj.add("aaa").add("bbb");
+		String scString = sj.toString();
+		System.out.println(scString);
+		
+	}
 }
