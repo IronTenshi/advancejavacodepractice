a51bbd9f3a2f29a44d3f2f721d5364f06f9ea300
IronTenshi <2356128385@qq.com>
2025-11-20 23:51:26 +0800

    Update test1.java

--- src/arraylist/test1.java
+++ src/arraylist/test1.java
@@ -1,5 +1,19 @@
 package arraylist;
 
-public class test1 {
+import java.util.ArrayList;
 
+public class test1 {
+	public static void main(String[] args) {
+		ArrayList<Integer> list1 = new ArrayList<Integer>();
+		boolean b = list1.add(1);
+		boolean c = list1.add(2);
+		System.out.println(b && c);
+		System.out.println(list1);
+		
+		list1.remove(1);
+		System.out.println(list1);
+		list1.set(0 , 333);
+		System.out.println(list1);
+		System.out.println(list1.get(0));
+	}
 }
