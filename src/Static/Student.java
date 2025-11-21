0524fca483624bcbc9225fad757c360c7c1d6424
IronTenshi <2356128385@qq.com>
2025-11-21 22:40:30 +0800

    1

---
+++ src/Static/Student.java
@@ -0,0 +1,45 @@
+package Static;
+
+
+public class Student {
+	private String name;
+	private int age;
+	private char gender;
+	private static String teacherName;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public char getGender() {
+		return gender;
+	}
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+	public Student(String name, int age, char gender) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+	}
+	public void display()
+	{
+		System.out.println("name : "+ name + "age  : "+ age + "gender : " +gender);
+	}
+	public void setTechername(String teacherName)
+	{
+		Student.teacherName = teacherName;
+	}
+	public void displayTecher()
+	{
+		System.out.println(teacherName);
+	}
+}
--- src/Static/test1.java
+++ src/Static/test1.java
@@ -2,6 +2,12 @@ package Static;
 
 public class test1 {
 	public static void main(String[] args) {
+		Student s1 = new Student("sb", 12, '男');
+		Student s2 = new Student("shachou", 23, '女');
 		
+		s1.setTechername("sdsd");
+		s1.display();
+		s2.display();
+		s2.displayTecher();
 	}
 }
