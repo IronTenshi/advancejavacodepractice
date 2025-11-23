94e48732f4fbb550ef7e764611d3b1f1925c3dac
IronTenshi <2356128385@qq.com>
2025-11-23 23:56:16 +0800

    add

---
+++ src/codepart/test2.java
@@ -0,0 +1,12 @@
+package codepart;
+
+public class test2 {
+	public static void main(String[] args) {
+		Student student = new Student("Sdf", 123);
+		{
+			System.out.println("create a student");
+		}
+		
+		student.display();
+	}
+}
