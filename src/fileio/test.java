22bd2cfb45d56606ff282d367e8ea8cbdcb9af07
IronTenshi <2356128385@qq.com>
2025-11-27 00:58:52 +0800

    Update test.java

--- src/fileio/test.java
+++ src/fileio/test.java
@@ -1,5 +1,12 @@
 package fileio;
 
-public class test {
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+public class test {
+	public static void main(String[] args) throws IOException {
+		FileOutputStream fos1 = new FileOutputStream("D:\\Testtxt\\test.txt");
+		fos1.write('3');
+		fos1.close();
+	}
 }
