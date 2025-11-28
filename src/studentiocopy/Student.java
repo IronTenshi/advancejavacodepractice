ece74d5f07137da5541e603d0e565884847b1024
IronTenshi <2356128385@qq.com>
2025-11-28 23:40:06 +0800

    Create Student.java

---
+++ src/studentiocopy/Student.java
@@ -0,0 +1,56 @@
+package studentiocopy;
+
+public class Student {
+	private String nameString;
+	private int age;
+	private String classsString;
+	private int mathScore;
+	private int engScore;
+	private int poScore;
+	public String getNameString() {
+		return nameString;
+	}
+	public void setNameString(String nameString) {
+		this.nameString = nameString;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getClasssString() {
+		return classsString;
+	}
+	public void setClasssString(String classsString) {
+		this.classsString = classsString;
+	}
+	public int getMathScore() {
+		return mathScore;
+	}
+	public void setMathScore(int mathScore) {
+		this.mathScore = mathScore;
+	}
+	public int getEngScore() {
+		return engScore;
+	}
+	public void setEngScore(int engScore) {
+		this.engScore = engScore;
+	}
+	public int getPoScore() {
+		return poScore;
+	}
+	public void setPoScore(int poScore) {
+		this.poScore = poScore;
+	}
+	public Student(String nameString, int age, String classsString, int mathScore, int engScore, int poScore) {
+		super();
+		this.nameString = nameString;
+		this.age = age;
+		this.classsString = classsString;
+		this.mathScore = mathScore;
+		this.engScore = engScore;
+		this.poScore = poScore;
+	}
+	
+}
